d5d861301bc0cc77c8f36b80c2dae3b61a237131
Anorme <anormeinkumsah@gmail.com>
2025-06-09 17:54:58 +0000

    Implement clear item from cart

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -36,12 +36,15 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
     );
 };
 
+const clearCartItem = (cartItems, cartItemToClear) => cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
   removeItemFromCart: () => {},
+  clearItemFromCart: () => {},
   cartCount: 0,
 });
 
@@ -61,9 +64,21 @@ export const CartProvider = ({ children }) => {
 
   const removeItemFromCart = (cartItemToRemove) => {
     setCartItems(removeCartItem(cartItems, cartItemToRemove));
+  };
+
+  const clearItemFromCart = (cartItemToClear) => {
+    setCartItems(clearCartItem(cartItems, cartItemToClear));
   }
 
-  const value = { isCartOpen, setIsCartOpen, addItemToCart, removeItemFromCart, cartItems, cartCount };
+  const value = { 
+    isCartOpen, 
+    setIsCartOpen, 
+    addItemToCart, 
+    removeItemFromCart, 
+    cartItems, 
+    cartCount, 
+    clearItemFromCart 
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
