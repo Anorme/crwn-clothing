b8fcf46192232dbc6b36cc9f77304664f1a2ddb3
Anorme <anormeinkumsah@gmail.com>
2025-06-16 00:59:53 +0000

    Refactor Selectors for Category State Management Using Reselect

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,19 @@
-export const selectCategoriesMap = (state) => state.categories.categories
-  .reduce((acc, docSnapshot) => {
-    const { title, items } = docSnapshot
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer], 
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => 
+    categories.reduce((acc, docSnapshot) => {
+      const { title, items } = docSnapshot
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {}
+  )
+);
