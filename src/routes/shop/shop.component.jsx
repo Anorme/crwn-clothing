c154ba9b7c6385ada82d442719986133922b18f4
Anorme <anormeinkumsah@gmail.com>
2025-06-17 11:30:09 +0000

    Use new data fetching thunk

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,17 +5,13 @@ import { Routes, Route } from 'react-router-dom';
 import Category from '../category/category.component';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 
-import { setCategories } from '../../store/categories/category.action';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+import { fetchCategoriesAsync } from '../../store/categories/category.action';
+
 
 const Shop = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments();
-      dispatch(setCategories(categoriesArray));
-    }
-    getCategoriesMap();
+    dispatch(fetchCategoriesAsync());
   }, []);
   
   return (
